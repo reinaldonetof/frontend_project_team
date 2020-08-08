fe3520937774d311f554fa40cd5ade99495500bd
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-08-07 23:37:31 -0300

    first redux saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Routes from "./routes";
+import { Provider } from "react-redux";
 
-const App = () => <Routes />;
+import store from "./store";
+
+import GlobalStyle from "./styles/global";
+
+const App = () => (
+  <Provider store={store}>
+    <Fragment>
+      <Routes />
+      <GlobalStyle />
+    </Fragment>
+  </Provider>
+);
 
 export default App;
---
+++ src/store/ducks/auth.js
@@ -0,0 +1,24 @@
+import { createReducer, createActions } from "reduxsauce";
+import Immutable from "seamless-immutable";
+
+const { Types, Creators } = createActions({
+  signInRequest: ["email", "password"],
+  signInSuccess: ["token"],
+});
+
+export const AuthTypes = Types;
+export default Creators;
+
+export const INITIAL_STATE = Immutable({
+  signedIn: false,
+  token: null,
+});
+
+export const success = (state, { token }) => {
+  console.log(token);
+  return state.merge({ signIn: true, token });
+};
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SIGN_IN_SUCCESS]: success,
+});
