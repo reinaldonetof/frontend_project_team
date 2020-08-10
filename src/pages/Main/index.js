95a37edd5428ce85889903fead2de38b96abfb74
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-08-09 22:21:39 -0300

    request team

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import api from "~/services/api";
 
-const Main = () => <div />;
+import TeamSwitcher from "~/components/TeamSwitcher";
+import { Container } from "./styles";
 
-export default Main;
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <TeamSwitcher />
+      </Container>
+    );
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { ReactReduxContext } from "react-redux";
 
 import history from "./history";
+
+import Private from "./private";
+import Guest from "./guest";
 
 import Main from "~/pages/Main";
 import SignIn from "~/pages/Auth/SignIn";
@@ -12,9 +15,9 @@ import SignUp from "~/pages/Auth/SignUp";
 const Routes = () => (
   <ConnectedRouter history={history} context={ReactReduxContext}>
     <Switch>
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/" component={Main} />
+      <Guest path="/signin" component={SignIn} />
+      <Guest path="/signup" component={SignUp} />
+      <Private path="/" component={Main} />
     </Switch>
   </ConnectedRouter>
 );
