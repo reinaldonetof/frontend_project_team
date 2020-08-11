4e828d75c62d1dbea0d8beb429c72f2a9f510e99
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-08-10 22:43:46 -0300

    making redux saga for new teams

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -5,12 +5,18 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import TeamsActions from "~/store/ducks/teams";
 
-import { Container, TeamList, Team } from "./styles";
+import Button from "~/styles/components/Button";
+import Modal from "~/components/Modal";
+
+import { Container, TeamList, Team, NewTeam } from "./styles";
 
 class TeamSwitcher extends Component {
   static propTypes = {
     getTeamsRequest: PropTypes.func.isRequired,
     selectTeam: PropTypes.func.isRequired,
+    openTeamModal: PropTypes.func.isRequired,
+    closeTeamModal: PropTypes.func.isRequired,
+    createTeamRequest: PropTypes.func.isRequired,
     teams: PropTypes.shape({
       data: PropTypes.arrayOf(
         PropTypes.shape({
@@ -19,6 +25,10 @@ class TeamSwitcher extends Component {
         })
       ),
     }).isRequired,
+  };
+
+  state = {
+    newTeam: "",
   };
 
   componentDidMount() {
@@ -33,8 +43,22 @@ class TeamSwitcher extends Component {
     selectTeam(team);
   };
 
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleCreateTeam = (e) => {
+    e.preventDefault();
+
+    const { createTeamRequest } = this.props;
+    const { newTeam } = this.state;
+
+    createTeamRequest(newTeam);
+  };
+
   render() {
-    const { teams } = this.props;
+    const { teams, openTeamModal, closeTeamModal } = this.props;
+    const { newTeam } = this.state;
 
     return (
       <Container>
@@ -47,6 +71,29 @@ class TeamSwitcher extends Component {
               />
             </Team>
           ))}
+
+          <NewTeam onClick={openTeamModal}>NOVO</NewTeam>
+
+          {teams.teamModalOpen && (
+            <Modal>
+              <h1>Criar time</h1>
+              <form onSubmit={this.handleCreateTeam}>
+                <span>NOME</span>
+                <input
+                  name="newTeam"
+                  value={newTeam}
+                  onChange={this.handleInputChange}
+                />
+
+                <Button size="big" type="submit">
+                  Salvar
+                </Button>
+                <Button onClick={closeTeamModal} size="small" color="gray">
+                  Cancelar
+                </Button>
+              </form>
+            </Modal>
+          )}
         </TeamList>
       </Container>
     );
--- src/store/ducks/teams.js
+++ src/store/ducks/teams.js
@@ -7,6 +7,8 @@ const { Types, Creators } = createActions({
   selectTeam: ["team"],
   openTeamModal: null,
   closeTeamModal: null,
+  createTeamRequest: ["name"],
+  createTeamSuccess: ["team"],
 });
 
 export const TeamsTypes = Types;
@@ -28,9 +30,13 @@ export const selectTeam = (state, { team }) => {
 export const openModal = (state) => state.merge({ teamModalOpen: true });
 export const closeModal = (state) => state.merge({ teamModalOpen: false });
 
+export const createSuccess = (state, { team }) =>
+  state.merge({ data: [...state.data, team] });
+
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.GET_TEAMS_SUCCESS]: getSuccess,
   [Types.SELECT_TEAM]: selectTeam,
   [Types.OPEN_TEAM_MODAL]: openModal,
   [Types.CLOSE_TEAM_MODAL]: closeModal,
+  [Types.CREATE_TEAM_SUCCESS]: createSuccess,
 });
