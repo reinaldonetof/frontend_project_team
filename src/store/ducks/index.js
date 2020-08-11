d5e168f696a47d25694f5bd4a742fa9d571545c8
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-08-10 22:01:27 -0300

    select team redux saga

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -3,12 +3,14 @@ import { connectRouter } from "connected-react-router";
 
 import { reducer as toastr } from "react-redux-toastr";
 import { reducer as auth } from "./auth";
+import { reducer as teams } from "./teams";
 
 const rootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
     auth,
     toastr,
+    teams,
   });
 
 export default rootReducer;
