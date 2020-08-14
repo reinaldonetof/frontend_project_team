0f5081b553a6468b4ec262627cd8ec5c557009cb
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-08-14 00:10:55 -0300

    create project saga

--- src/store/ducks/projects.js
+++ src/store/ducks/projects.js
@@ -6,6 +6,8 @@ const { Types, Creators } = createActions({
   getProjectsSuccess: ["data"],
   openProjectModal: null,
   closeProjectModal: null,
+  createProjectRequest: ["title"],
+  createProjectSuccess: ["project"],
 });
 
 export const ProjectsTypes = Types;
@@ -22,8 +24,12 @@ export const openModal = (state) => state.merge({ projectModalOpen: true });
 
 export const closeModal = (state) => state.merge({ projectModalOpen: false });
 
+export const createSuccess = (state, { project }) =>
+  state.merge({ data: [...state.data, project] });
+
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.GET_PROJECTS_SUCCESS]: success,
   [Types.OPEN_PROJECT_MODAL]: openModal,
   [Types.CLOSE_PROJECT_MODAL]: closeModal,
+  [Types.CREATE_PROJECT_SUCCESS]: createSuccess,
 });
--- src/store/sagas/projects.js
+++ src/store/sagas/projects.js
@@ -1,4 +1,5 @@
 import { call, put } from "redux-saga/effects";
+import { action as toastrActions } from "react-redux-toastr";
 import api from "~/services/api";
 
 import ProjectActions from "../ducks/projects";
@@ -7,4 +8,20 @@ export function* getProjects() {
   const response = yield call(api.get, "projects");
 
   yield put(ProjectActions.getProjectsSuccess(response.data));
+}
+
+export function* createProject({ title }) {
+  try {
+    const response = yield call(api.post, "projects", { title });
+    yield put(ProjectActions.createProjectSuccess(response.data));
+    yield put(ProjectActions.closeProjectModal());
+  } catch (e) {
+    yield put(
+      toastrActions.add({
+        type: "error",
+        title: "Erro na operação",
+        message: "Houve um erro, tente novamente!",
+      })
+    );
+  }
 }
